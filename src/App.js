7f57eaf6fd7ec004539b7aa7060c80656c874583
jacobhey235 <66252989+jacobhey235@users.noreply.github.com>
2024-06-06 22:16:58 +0300

    fixed missing tg object

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import {useEffect} from "react";
+const tg = window.Telegram.WebApp;
 function App() {
+
+    useEffect(() => {
+        tg.ready()
+    }, []);
   return (
     <div className="App">
       working
