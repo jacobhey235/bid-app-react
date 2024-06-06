38c11718e0f8a5a4378d2cd9dc097217c95a6fb2
jacobhey235 <66252989+jacobhey235@users.noreply.github.com>
2024-06-06 22:15:12 +0300

    init1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import {useEffect} from "react";
 function App() {
-
-    useEffect(() => {
-        tg.ready()
-    }, []);
   return (
     <div className="App">
       working
